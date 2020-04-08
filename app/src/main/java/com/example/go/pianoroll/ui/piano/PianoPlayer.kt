af3af0f74442f42b2e575daeea577b74bdd2192f
go shibata <warahiko@gmail.com>
2020-04-09 02:20:52 +0900

    Fix: 遅延解消

--- app/src/main/java/com/example/go/pianoroll/ui/piano/PianoPlayer.kt
+++ app/src/main/java/com/example/go/pianoroll/ui/piano/PianoPlayer.kt
@@ -1,8 +1,8 @@
 package com.example.go.pianoroll.ui.piano
 
 import android.content.Context
-import android.media.MediaPlayer
-import androidx.annotation.RawRes
+import android.media.AudioAttributes
+import android.media.SoundPool
 import androidx.core.content.res.getResourceIdOrThrow
 import com.example.go.pianoroll.R
 
@@ -14,71 +14,32 @@ class PianoPlayer(
         private const val START_NOTE_NUMBER = 60
     }
 
-    private val player = MediaPlayerPool(context, 24)
-    private val notes: Map<Int, Int>
+    private val pool: SoundPool
+    private val noteToSound: Map<Int, Int>
 
     init {
+        val attr = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        pool = SoundPool.Builder()
+            .setAudioAttributes(attr)
+            .setMaxStreams(24)
+            .build()
+
         val notesArray = context.resources.obtainTypedArray(R.array.notes)
-        notes = (0 until notesArray.length()).associate {
-            START_NOTE_NUMBER + it to notesArray.getResourceIdOrThrow(it)
+        noteToSound = (0 until notesArray.length()).associate {
+            START_NOTE_NUMBER + it to pool.load(context, notesArray.getResourceIdOrThrow(it), 1)
         }
         notesArray.recycle()
     }
 
     fun play(note: Int) {
-        val noteRes = notes[note] ?: return
-        player.playSound(noteRes)
+        val sound = noteToSound[note] ?: return
+        pool.play(sound, 1f, 1f, 0, 0, 1f)
     }
 
     fun onDestroy() {
-        player.onDestroy()
-    }
-
-    class MediaPlayerPool(context: Context, maxStreams: Int) {
-        private val context: Context = context.applicationContext
-
-        private val mediaPlayerPool = (0 until maxStreams).map { buildPlayer() }.toMutableList()
-        private val playersInUse = mutableListOf<MediaPlayer>()
-
-        private fun buildPlayer() = MediaPlayer().apply {
-            setOnPreparedListener { start() }
-            setOnCompletionListener { recyclePlayer(it) }
-        }
-
-        /**
-         * Returns a [MediaPlayer] if one is available,
-         * otherwise null.
-         */
-        private fun requestPlayer(): MediaPlayer? {
-            return if (mediaPlayerPool.isNotEmpty()) {
-                mediaPlayerPool.removeAt(0).also {
-                    playersInUse += it
-                }
-            } else null
-        }
-
-        private fun recyclePlayer(mediaPlayer: MediaPlayer) {
-            mediaPlayer.reset()
-            playersInUse -= mediaPlayer
-            mediaPlayerPool += mediaPlayer
-        }
-
-        fun playSound(@RawRes rawResId: Int) {
-            val assetFileDescriptor = context.resources.openRawResourceFd(rawResId) ?: return
-            val mediaPlayer = requestPlayer() ?: return
-
-            mediaPlayer.run {
-                setDataSource(
-                    assetFileDescriptor.fileDescriptor, assetFileDescriptor.startOffset,
-                    assetFileDescriptor.declaredLength
-                )
-                prepareAsync()
-            }
-        }
-
-        fun onDestroy() {
-            mediaPlayerPool.forEach { it.release() }
-            playersInUse.forEach { it.release() }
-        }
+        pool.release()
     }
 }
