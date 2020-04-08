13b1644bdf1bc21055050ebd693863c484c8a74f
go shibata <warahiko@gmail.com>
2020-04-08 18:27:03 +0900

    Fix: 多くの MediaPlayer インスタンスを作るとエラーで落ちる問題の修正

--- app/src/main/java/com/example/go/pianoroll/ui/piano/PianoPlayer.kt
+++ app/src/main/java/com/example/go/pianoroll/ui/piano/PianoPlayer.kt
@@ -2,6 +2,7 @@ package com.example.go.pianoroll.ui.piano
 
 import android.content.Context
 import android.media.MediaPlayer
+import androidx.annotation.RawRes
 import androidx.core.content.res.getResourceIdOrThrow
 import com.example.go.pianoroll.R
 
@@ -13,36 +14,71 @@ class PianoPlayer(
         private const val START_NOTE_NUMBER = 60
     }
 
-    private val players: Map<Int, MediaPlayer>
+    private val player = MediaPlayerPool(context, 24)
+    private val notes: Map<Int, Int>
 
     init {
-        val notes = context.resources.obtainTypedArray(R.array.notes)
-        players = (0 until notes.length()).associate {
-            START_NOTE_NUMBER + it to MediaPlayer.create(context, notes.getResourceIdOrThrow(it))
+        val notesArray = context.resources.obtainTypedArray(R.array.notes)
+        notes = (0 until notesArray.length()).associate {
+            START_NOTE_NUMBER + it to notesArray.getResourceIdOrThrow(it)
         }
-        notes.recycle()
+        notesArray.recycle()
     }
 
     fun play(note: Int) {
-        players[note]?.run {
-            seekTo(0)
-            start()
-        }
-    }
-
-    fun stop(note: Int) {
-        players[note]?.run {
-            pause()
-        }
-    }
-
-    fun isPlaying(note: Int): Boolean {
-        return players[note]?.isPlaying ?: false
+        val noteRes = notes[note] ?: return
+        player.playSound(noteRes)
     }
 
     fun onDestroy() {
-        players.forEach {
-            it.value.release()
+        player.onDestroy()
+    }
+
+    class MediaPlayerPool(context: Context, maxStreams: Int) {
+        private val context: Context = context.applicationContext
+
+        private val mediaPlayerPool = (0 until maxStreams).map { buildPlayer() }.toMutableList()
+        private val playersInUse = mutableListOf<MediaPlayer>()
+
+        private fun buildPlayer() = MediaPlayer().apply {
+            setOnPreparedListener { start() }
+            setOnCompletionListener { recyclePlayer(it) }
+        }
+
+        /**
+         * Returns a [MediaPlayer] if one is available,
+         * otherwise null.
+         */
+        private fun requestPlayer(): MediaPlayer? {
+            return if (mediaPlayerPool.isNotEmpty()) {
+                mediaPlayerPool.removeAt(0).also {
+                    playersInUse += it
+                }
+            } else null
+        }
+
+        private fun recyclePlayer(mediaPlayer: MediaPlayer) {
+            mediaPlayer.reset()
+            playersInUse -= mediaPlayer
+            mediaPlayerPool += mediaPlayer
+        }
+
+        fun playSound(@RawRes rawResId: Int) {
+            val assetFileDescriptor = context.resources.openRawResourceFd(rawResId) ?: return
+            val mediaPlayer = requestPlayer() ?: return
+
+            mediaPlayer.run {
+                setDataSource(
+                    assetFileDescriptor.fileDescriptor, assetFileDescriptor.startOffset,
+                    assetFileDescriptor.declaredLength
+                )
+                prepareAsync()
+            }
+        }
+
+        fun onDestroy() {
+            mediaPlayerPool.forEach { it.release() }
+            playersInUse.forEach { it.release() }
         }
     }
 }
