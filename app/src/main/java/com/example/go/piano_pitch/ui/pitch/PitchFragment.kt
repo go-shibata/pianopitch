6c953bed621db96f54bcb98c87284fc000e55d56
go shibata <warahiko@gmail.com>
2020-04-15 02:22:08 +0900

    Update: PitchFragment で弾いた音の複数表示

--- app/src/main/java/com/example/go/piano_pitch/ui/pitch/PitchFragment.kt
+++ app/src/main/java/com/example/go/piano_pitch/ui/pitch/PitchFragment.kt
@@ -2,11 +2,17 @@ package com.example.go.piano_pitch.ui.pitch
 
 import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.go.piano_pitch.R
 import com.example.go.piano_pitch.databinding.FragmentPitchBinding
 import com.example.go.piano_pitch.di.ViewModelFactory
 import com.example.go.piano_pitch.ui.view.piano.PianoView
@@ -19,6 +25,8 @@ class PitchFragment : Fragment(), PianoView.OnPlayListener {
     lateinit var factory: ViewModelFactory<PitchViewModel>
     private val viewModel: PitchViewModel by viewModels { factory }
 
+    private lateinit var binding: FragmentPitchBinding
+
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
@@ -28,12 +36,33 @@ class PitchFragment : Fragment(), PianoView.OnPlayListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentPitchBinding.inflate(inflater, container, false).apply {
+        binding = FragmentPitchBinding.inflate(inflater, container, false).apply {
             viewModel = this@PitchFragment.viewModel
             lifecycleOwner = viewLifecycleOwner
             piano.setOnPlayListener(this@PitchFragment)
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.playedNoteName.observe(viewLifecycleOwner, Observer {
+            val layoutParams = LinearLayout.LayoutParams(
+                resources.getDimensionPixelSize(R.dimen.pitch_notes_size),
+                resources.getDimensionPixelSize(R.dimen.pitch_notes_size)
+            )
+            val textView = TextView(requireContext()).apply {
+                setLayoutParams(layoutParams)
+                gravity = Gravity.CENTER
+                text = it
+                setTextSize(
+                    TypedValue.COMPLEX_UNIT_PX,
+                    resources.getDimension(R.dimen.pitch_notes_text_size)
+                )
+            }
+            binding.aNotes.addView(textView)
+        })
     }
 
     override fun onPlay(note: Int) {
