dd78670b92596191048eaabf9d1de6801d107b13
go shibata <warahiko@gmail.com>
2020-04-15 00:37:13 +0900

    Add: ManuFragment

--- app/src/main/java/com/example/go/piano_pitch/ui/MainActivity.kt
+++ app/src/main/java/com/example/go/piano_pitch/ui/MainActivity.kt
@@ -3,17 +3,11 @@ package com.example.go.piano_pitch.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.go.piano_pitch.R
-import com.example.go.piano_pitch.ui.piano.PianoFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, PianoFragment.newInstance())
-                .commitNow()
-        }
     }
 }
---
+++ app/src/main/java/com/example/go/piano_pitch/ui/menu/MenuFragment.kt
@@ -0,0 +1,28 @@
+package com.example.go.piano_pitch.ui.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.go.piano_pitch.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMenuBinding.inflate(inflater, container, false).apply {
+            fragment = this@MenuFragment
+        }
+        return binding.root
+    }
+
+    fun onClickPianoButton() {
+        findNavController().navigate(
+            MenuFragmentDirections.actionMenuFragmentToPianoFragment()
+        )
+    }
+}
