cfe1aec457cfe59472e45cfebcb5ba4a301c82ef
go shibata <warahiko@gmail.com>
2020-04-16 02:20:23 +0900

    Update: navigation with ActionBar Update: title of Fragment

--- app/src/main/java/com/example/go/piano_pitch/ui/MainActivity.kt
+++ app/src/main/java/com/example/go/piano_pitch/ui/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.go.piano_pitch.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.go.piano_pitch.R
 import dagger.android.AndroidInjection
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
