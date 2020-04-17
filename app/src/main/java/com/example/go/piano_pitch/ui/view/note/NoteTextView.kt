3ba82d80150b800357634b458b9fc0fe1ec55df7
go shibata <warahiko@gmail.com>
2020-04-18 01:50:09 +0900

    Refactor: NoteTextView

--- app/src/main/java/com/example/go/piano_pitch/ui/view/note/NoteTextView.kt
+++ app/src/main/java/com/example/go/piano_pitch/ui/view/note/NoteTextView.kt
@@ -1,7 +1,7 @@
 package com.example.go.piano_pitch.ui.view.note
 
 import android.content.Context
-import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import com.example.go.piano_pitch.R
 import kotlinx.android.synthetic.main.text_note.view.*
@@ -14,9 +14,7 @@ class NoteTextView(
     constructor(context: Context) : this(context, "")
 
     init {
-        val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.text_note, this, true)
-
+        val view = View.inflate(context, R.layout.text_note, this)
         (view as FrameLayout).text.text = text
     }
 }
