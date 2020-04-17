221da2bde5af9f3fd6379666a9873590ece8a5b9
go shibata <warahiko@gmail.com>
2020-04-17 22:35:57 +0900

    Add: AnyToAny

--- app/src/main/java/com/example/go/piano_pitch/logic/PitchType.kt
+++ app/src/main/java/com/example/go/piano_pitch/logic/PitchType.kt
@@ -1,5 +1,6 @@
 package com.example.go.piano_pitch.logic
 
+import com.example.go.piano_pitch.logic.usecase.AnyToAny
 import com.example.go.piano_pitch.logic.usecase.CToAny
 import com.example.go.piano_pitch.logic.usecase.CToWhite
 import com.example.go.piano_pitch.logic.usecase.PitchTypeBase
@@ -11,7 +12,9 @@ enum class PitchType(
     val title: String
 ) {
     C_TO_WHITE(CToWhite::class, "二音（ド→白鍵）"),
-    C_TO_ANY(CToAny::class, "二音（ド→全て）");
+    C_TO_ANY(CToAny::class, "二音（ド→全て）"),
+    ANY_TO_ANY(AnyToAny::class, "二音"),
+    ;
 
     fun sample(): List<Int> = (className.objectInstance as PitchTypeBase).sample()
 }
---
+++ app/src/main/java/com/example/go/piano_pitch/logic/usecase/AnyToAny.kt
@@ -0,0 +1,23 @@
+package com.example.go.piano_pitch.logic.usecase
+
+import com.example.go.piano_pitch.logic.InitializeProbability
+import com.example.go.piano_pitch.logic.MarkovChain
+import com.example.go.piano_pitch.logic.TransitionProbability
+
+@ExperimentalStdlibApi
+object AnyToAny : PitchTypeBase() {
+
+    override val markovChain: MarkovChain
+
+    init {
+        val init = List(12) { 1.0 / 12 }
+
+        val trans = List(12) {
+            List(12) { 1.0 / 12 }
+        }
+
+        markovChain = MarkovChain(InitializeProbability(init), TransitionProbability(trans))
+    }
+
+    override fun sample(): List<Int> = markovChain.sample(2)
+}
