a291e3e021a134123d3a85a1cbe01efd64876df6
go shibata <warahiko@gmail.com>
2020-04-17 23:13:09 +0900

    Add: WhiteToWhite

--- app/src/main/java/com/example/go/piano_pitch/logic/PitchType.kt
+++ app/src/main/java/com/example/go/piano_pitch/logic/PitchType.kt
@@ -10,6 +10,7 @@ enum class PitchType(
 ) {
     C_TO_WHITE(CToWhite::class, "二音（ド→白鍵）"),
     C_TO_ANY(CToAny::class, "二音（ド→全て）"),
+    WHITE_TO_WHITE(WhiteToWhite::class, "二音（白鍵→白鍵）"),
     ANY_TO_NEXT(AnyToNext::class, "二音（隣同士）"),
     ANY_TO_ANY(AnyToAny::class, "二音"),
     ;
