9d063753dbe3c4caa06fe16767cafcdda0864709
go shibata <warahiko@gmail.com>
2020-04-18 23:01:33 +0900

    Fix: single note becomes C

--- app/src/main/java/com/example/go/piano_pitch/logic/usecase/PitchTypeBase.kt
+++ app/src/main/java/com/example/go/piano_pitch/logic/usecase/PitchTypeBase.kt
@@ -5,14 +5,15 @@ import com.example.go.piano_pitch.logic.MarkovChain
 abstract class PitchTypeBase {
 
     companion object {
-        fun List<Int>.asIndexList(vararg size: Int = IntArray(1) { 1 }): List<List<Int>> = this.map {
-            var index = it
-            size.reversed().map { s ->
-                val value = index % s
-                index /= s
-                value
-            }.reversed()
-        }
+        fun List<Int>.asIndexList(vararg size: Int = IntArray(1) { 12 }): List<List<Int>> =
+            this.map {
+                var index = it
+                size.reversed().map { s ->
+                    val value = index % s
+                    index /= s
+                    value
+                }.reversed()
+            }
     }
 
     @ExperimentalStdlibApi
