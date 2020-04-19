f0a72c6413becfd912f71de07ad676ace5d529ec
go shibata <warahiko@gmail.com>
2020-04-19 21:56:20 +0900

    Update: init pitchTypeTable in MainActivity

--- app/src/main/java/com/example/go/piano_pitch/ui/MainActivity.kt
+++ app/src/main/java/com/example/go/piano_pitch/ui/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.go.piano_pitch.R
+import com.example.go.piano_pitch.data.database.MyDatabase
+import com.example.go.piano_pitch.data.database.entity.PitchType
 import com.example.go.piano_pitch.di.ViewModelFactory
 import dagger.android.AndroidInjection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -17,16 +22,31 @@ class MainActivity : AppCompatActivity() {
     lateinit var factory: ViewModelFactory<MainActivityViewModel>
     private val viewModel: MainActivityViewModel by viewModels { factory }
 
+    @Inject
+    lateinit var myDatabase: MyDatabase
+
     private lateinit var navController: NavController
 
+    @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         viewModel // to init
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        initDatabasePitchType()
+
         navController = findNavController(R.id.nav_host_fragment)
         setupActionBarWithNavController(navController)
+    }
+
+    @ExperimentalStdlibApi
+    fun initDatabasePitchType() {
+        CoroutineScope(Dispatchers.Default).launch {
+            com.example.go.piano_pitch.logic.PitchType.values().forEach {
+                myDatabase.pitchTypeDao().insert(PitchType(it.id, 0))
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
